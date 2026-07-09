56995caf89e4ede8edb3788d37bcb52aaef3f7bb
JoseySR <118849789+Billonario77@users.noreply.github.com>
2026-07-09 11:56:48 -0500

    Fix API_URL for React Scripts

--- frontend/src/services/turnosService.ts
+++ frontend/src/services/turnosService.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 //const API_URL = 'http://localhost:3001/api';
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+
+//const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api';
 
 export interface Turno {
   id: string;
